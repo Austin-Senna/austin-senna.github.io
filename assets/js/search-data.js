757d91afc6622eb87062967a6b5e2b087454ed65
Austin-Senna <51234553+Austin-Senna@users.noreply.github.com>
2026-07-09 02:45:41 +0000

    Deploying to gh-pages from @ Austin-Senna/austin-senna.github.io@c50214fcf0eac3b66a037d36df77022a9d46dbb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-experience",
           title: "experience",
           description: "Research, software engineering, and data engineering roles.",
